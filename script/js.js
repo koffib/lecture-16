6c116aef862ca9bf606d077688759e11bd01760a
koffib <kbecanty@student.rtc.edu>
2022-06-02 10:19:20 -0700

    Initial commit

---
+++ script/js.js
@@ -0,0 +1,27 @@
+//loop
+//condition
+//variables
+//operators
+//input
+//output
+
+
+
+//variables
+const constant = 0;
+var dontDovar= 5;
+let dolet = 10;
+
+//conditions
+if(true){
+
+}
+else if(true){
+
+}
+else{
+
+}
+switch('1'){
+    
+}
