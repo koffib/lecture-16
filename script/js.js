a674affd08abc747492cbefc43eee3d6ebb4fbce
koffib <kbecanty@student.rtc.edu>
2022-06-02 11:13:20 -0700

    updated

--- script/js.js
+++ script/js.js
@@ -1,4 +1,24 @@
+window.onload = function(){
+
+    const btnDisplay = document.getElementById("btnDisplay");
+    const numbers = document.getElementsByClassName("number")
+    
+    btnDisplay.addEventListener('click', function(){
+      let num1 =  numbers[0].value;
+      let num2 = numbers[1];
+      let total = addtwoNumbers(num1, num2);
+    })
+}
+
+
+
+
+
+
 //loop
+
+
+
 //condition
 //variables
 //operators
@@ -13,15 +33,71 @@ var dontDovar= 5;
 let dolet = 10;
 
 //conditions
-if(true){
+//if(true){
 
-}
-else if(true){
+//}
+//else if(true){
 
-}
-else{
+//}
+//else{
 
-}
-switch('1'){
+//}
+//switch('1'){
+ //   case"1":
+
+  //  break;
+  //  default:
+
+  //  break;
+//}
+
+//loop
+//for(let i = 0; i <= 10; i++){
     
-}
+//}
+
+//while(true){
+
+//}
+//for(;true;){
+
+
+
+//}
+
+//let sum =0;
+//const numbers =[65, 44, 12, 4];
+//numbers.forEach(myFunction);
+
+//operator
+//=== Compares type and value
+//== Compares value
+
+//Math Operators 
+//+ , -, *, /, %
+
+//composite operators
+//+=, -=, *=, /=, %=
+
+//equality operator
+// ==, !=
+
+//comparison operators
+//<,>, <=, =>
+
+//logical operators
+//&&, ||, !
+
+//Input
+//Getting information
+//input
+//textarea
+//buttons
+
+//output
+//Display information
+//
+
+let CompareExample = 5 ==="5"
+
+
